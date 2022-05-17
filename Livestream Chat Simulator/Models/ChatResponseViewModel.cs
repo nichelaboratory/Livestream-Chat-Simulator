b529d859eba1d7870c14417ef6857556f53158e5
Brett <brett.burridge@gmail.com>
2022-05-17 10:00:42 +0100

    Add project files.

---
+++ Livestream Chat Simulator/Models/ChatResponseViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Livestream_Chat_Simulator.Models
+{
+    public class ChatResponseViewModel
+    {
+        public string UserName { get; set; }
+        public string Response { get; set; }
+        public DateTime TimeStamp { get; set; }
+    }
+}
